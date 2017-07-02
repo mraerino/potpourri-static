fa45cf149cbf7ea3fb17465ba5ca46d8bec1b8b4
mraerino <melodie124@gmail.com>
2017-07-02 16:09:17 +0200

    :tada: Adapt initial setup from victor-hugo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,9 @@
 import gulp from "gulp";
 import cp from "child_process";
-import gutil from "gulp-util";
 import postcss from "gulp-postcss";
 import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
-import webpack from "webpack";
-import webpackConfig from "./webpack.conf";
 
 const browserSync = BrowserSync.create();
 const hugoBin = "hugo";
@@ -15,8 +12,8 @@ const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
 
-gulp.task("build", ["css", "js", "hugo"]);
-gulp.task("build-preview", ["css", "js", "hugo-preview"]);
+gulp.task("build", ["css", "hugo"]);
+gulp.task("build-preview", ["css", "hugo-preview"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
@@ -25,27 +22,12 @@ gulp.task("css", () => (
     .pipe(browserSync.stream())
 ));
 
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
-
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
-});
-
-gulp.task("server", ["hugo", "css", "js"], () => {
+gulp.task("server", ["hugo", "css"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
     }
   });
-  gulp.watch("./src/js/**/*.js", ["js"]);
   gulp.watch("./src/css/**/*.css", ["css"]);
   gulp.watch("./site/**/*", ["hugo"]);
 });
